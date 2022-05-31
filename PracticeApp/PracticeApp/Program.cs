60830946a4932dcd97d8021b223f14519ea44150
1110tanmay <31026364+1110tanmay@users.noreply.github.com>
2022-05-31 17:12:55 +0530

    Add files via upload

---
+++ PracticeApp/PracticeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace LearnString
+{
+    class Learnstrings 
+    {
+        static Boolean IsPaswordcorrect(string s)
+        {
+            return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+        }
+    public static void Main()
+        {
+            Console.WriteLine(IsPaswordcorrect("Hello123"));
+        }
+    }
+}
